9b19d82754bf7373eb3e18007b98f5132eb47af0
Kristoffer <klegind@gmail.com>
2022-02-10 15:15:55 +0100

    added parse_annotation_object

--- read_specimen_text.py
+++ read_specimen_text.py
@@ -7,23 +7,49 @@ from PIL import Image
 import cv2
 
 
-img = cv2.imread('specimen_images/dacty.png')
+# input_img = cv2.imread('specimen_images/dacty.png')
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "jkl-specimen-ocr-a940a8d4649b.json"
 client = vision.ImageAnnotatorClient()
 
 def convert_to_GV_client_format(image):
+    #image must be cv2.imread()
     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     success, encoded_image = cv2.imencode('.jpg', img)
     roi_image = encoded_image.tobytes()
     roi_image = vision.Image(content=roi_image)
     response = client.text_detection(image=roi_image)
     return response
-#
-img_for_GCV = convert_to_GV_client_format(img)
-
-for word in img_for_GCV.text_annotations:
-    d = {'text': word.description}
-    print(d)
 
 img = Image.open("specimen_images/dacty.png")
 img.show()
+
+##################
+# string processing of GCV text
+##################
+
+def parse_annotation_object(annotation_object):
+    '''Pull out the label text and convert it to a text
+    #string for further processing'''
+    labels = annotation_object.text_annotations[0].description
+
+    tt = []
+    # for j in labels:
+    #     tt.append(j)
+    [tt.append(elem) for elem in labels]
+    agg = []
+    listToStr = ''.join([str(elem) for elem in tt])
+
+    for j in listToStr:
+        if j == '\n':
+            agg.append('|')
+        else:
+            agg.append(j)
+
+    str_agg = ''.join([str(elem) for elem in agg])
+    return str_agg
+
+def execute_image_label_vision(input_img):
+    #input image must by cv2.imread()
+    annotation = convert_to_GV_client_format(input_img)
+    res = parse_annotation_object(annotation)
+    return res
