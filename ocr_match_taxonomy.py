151cf8c446ce9cc4acbd9c0996dfeba3f50838e3
Kristoffer <klegind@gmail.com>
2022-02-22 11:22:39 +0100

    updates and code cleanup

--- ocr_match_taxonomy.py
+++ ocr_match_taxonomy.py
@@ -1,47 +1,53 @@
 import requests
-import json
-from time import sleep
+
+#
+import sys
+sys.stdout = open(1, 'w', encoding='utf-8', closefd=False)
+#
 
 def get_GBIF_taxonomic_lookup(candidate_name, output_dict={}):
-    # A simple function to get the ''
+    # A simple function to get the 'GBIF interpreted taxonomic lookup' for a given name string
     api_url = 'https://api.gbif.org/v1/species/match?name={}'.format(candidate_name)
     response = requests.get(api_url)
     rson = response.json()
-    print(candidate_name)
+
     if rson['matchType'] != "NONE":
         print(rson['matchType'], type(rson['matchType']))
+        output_dict['submitted_string'] = candidate_name
         output_dict['scientific_name'] = rson['scientificName']
         output_dict['rank'] = rson['rank']
         output_dict['status'] = rson['status']
         output_dict['confidence_score'] = rson['confidence']
     return output_dict
-# res = get_GBIF_taxonomic_lookup('Dactylorhiza maculata')
-# print(res)
-labels = ['CLF104120', 'HERBIERS UNIVERSITAIRES DE CLERMONT-FERRAND CLF104120.', 'Dactylorhiza maculata', '(L.) SOÓ', 'subsp. maculata', 'Famille Orchidaceae', 'Identification Thébaud G.', 'Récolteur Thomas M.', 'Date 2012', 'Date 6/7/2012', 'Pays France', 'Départerment Puy-de-Dôme', 'Commune Le Monestier', 'Virennes (Le Monestier, 63), PSET n°2.', 'note', 'RECOLNAT', '']
 
-def pick_out_species_name(label_list):
+# labels = ['CLF104120', 'HERBIERS UNIVERSITAIRES DE CLERMONT-FERRAND CLF104120.', 'Dactylorhiza maculata', '(L.) SOÓ', 'subsp. maculata', 'Famille Orchidaceae', 'Identification Thébaud G.', 'Récolteur Thomas M.', 'Date 2012', 'Date 6/7/2012', 'Pays France', 'Départerment Puy-de-Dôme', 'Commune Le Monestier', 'Virennes (Le Monestier, 63), PSET n°2.', 'note', 'RECOLNAT', '']
+
+def look_up_species_names(labels, confidence_score=80):
     '''Based on the label list from the read_specimen_text module, this function looks up each element from the list in the GBIF taxonomic API via
      the get_GBIF_taxonomic_lookup() function.
     '''
     result_list = []
     for elem in labels:
-        # sleep(3)
+        # elem = elem.encode()
+        # elem = elem.decode()
         api_res = get_GBIF_taxonomic_lookup(elem)
         # print('for loop', api_res, len(api_res))
 
-        if ((len(api_res)>0) and (api_res['confidence_score'] > 80)):
-            print('High confidence result: ', api_res)
+        if ((len(api_res)>0) and (api_res['confidence_score'] > confidence_score)):
+            # print('High confidence result: ', api_res)
             result_list.append(api_res)
     return result_list
 
-race = pick_out_species_name(labels)
+# race = look_up_species_name(labels)
 
 def check_for_api_cache_issues(results):
     #API servers can have cache issues so that different API requests return the exact same response.
     #This function mitigates that issue. I see this as a bandaid rather than a solid solution.
-    result_list = [json.dumps(elem) for elem in race]
-    setres = list(set(result_list))
-    return setres.
+    result_list = [elem for elem in results]
 
-res = check_for_api_cache_issues(race)
-print('final res = ', res)
+    set_res = [dict(s) for s in set(frozenset(d.items()) for d in result_list)]
+    # Removes duplicates in the API result coming from API server caching issues
+    return set_res
+
+# res = check_for_api_cache_issues(race)
+# print('final res = ', res)
