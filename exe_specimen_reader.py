d3fef3cbac34e812fd2cfd946030ba934f0c5743
Kristoffer <klegind@gmail.com>
2022-02-23 12:58:47 +0100

    code cleanup

--- exe_specimen_reader.py
+++ exe_specimen_reader.py
@@ -16,32 +16,27 @@ def get_OCR_text(images_list):
         res = read_specimen_text.execute_image_label_vision(img)
         ocr_text = res[0]
         records.append(ocr_text)
-        print("List of OCR'ed text: ", records)
-        # cv2.imshow('input image {}-'.format(img_name), img)
-        # cv2.waitKey(0)
+        # print("List of OCR'ed text: ", records)
 
-        # print("labels??", type(labels_list), labels_list)
-        # records.append(labels_list)
     return records
-cropped_imgs = cv2_crawl_and_crop.exe_cropping('/home/stoffer/Pictures/images_for_pipeline')
-print('type Cropped IMGES: ', type(cropped_imgs))
-OCR_treated_text = get_OCR_text(cropped_imgs)
-print("get_CR_text out: ", OCR_treated_text)
 
-# for candidate in OCR_treated_text:
+##print statements for checking OCR result
+cropped_imgs = cv2_crawl_and_crop.exe_cropping('/home/stoffer/Pictures/images_for_pipeline')
+OCR_treated_text = get_OCR_text(cropped_imgs)
+for elem in OCR_treated_text:
+    print(elem , '\n')
+##end##
+
 candidates = OCR_treated_text
 print('Google Vision api call res: ', candidates)
-# species_names = ocr_match_taxonomy.look_up_species_names(candidates)
 
-# def obtain_
 tax_res = []
 for elem in candidates:
     res = ocr_match_taxonomy.look_up_species_names(elem)
     cleaned_res = ocr_match_taxonomy.check_for_api_cache_issues(res)
     tax_res.append(cleaned_res)
-# conf = ocr_match_taxonomy.check_for_api_cache_issues(species_names)
 
-for j in tax_res: print(j)
-# print('\n GBIF Taxonomic lookup == ', tax_res)
+for j in tax_res: print('Submitted candidate string /{}/ ='.format(j[0]['submitted_string']), j)
+
 
 
