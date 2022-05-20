17c3441b8b4affb65057d6a034ccf95b5bd05ae3
Jan Krzysztof Legind <bxq762@ku.dk>
2022-05-20 15:37:19 +0200

    overhaul

--- exe_specimen_reader.py
+++ exe_specimen_reader.py
@@ -1,30 +1,37 @@
 #Run the OCR workflow
 import read_specimen_text
-import cv2_crawl_and_crop
+import improved_crop
 import cv2
 import ocr_match_taxonomy
+import os
 
 
 def get_OCR_text(images_list):
     #param: list of images for OCR treatment
     #returns a list of lists (records) for further API lookups
     records = []
-    for key in cropped_imgs:
-        img_path = cropped_imgs[key]
+    print('images list == ', images_list)
+    for key in images_list:
+        img_path = images_list[key]
         img_name = key
+        print('path: {} and name: {}'.format(img_path, img_name))
         img = cv2.imread(img_path)
         res = read_specimen_text.execute_image_label_vision(img)
         ocr_text = res[0]
         records.append(ocr_text)
-        # print("List of OCR'ed text: ", records)
+        print("List of OCR'ed text: ", records)
 
     return records
 
 ##print statements for checking OCR result
-cropped_imgs = cv2_crawl_and_crop.exe_cropping('/home/stoffer/Pictures/images_for_pipeline')
+cropped_imgs = improved_crop.exe_cropping('C:/python_dassco/Specimens_OCR/specimen_images',
+                                               'C:/python_dassco/Specimens_OCR/cropped_images')
+
+print('cropped imagesssssss: ', cropped_imgs)
 OCR_treated_text = get_OCR_text(cropped_imgs)
 for elem in OCR_treated_text:
-    print(elem , '\n')
+    print('OCR text', elem , '\n')
+
 ##end##
 
 candidates = OCR_treated_text
